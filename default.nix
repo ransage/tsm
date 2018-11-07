9ad7f086444483e6814fb1f83eaf86693f78dee5
Sriram Sundararajan <sriram@tinverse.com>
2018-11-07 10:55:36 -0800

    Modified default.nix and related refactoring

--- default.nix
+++ default.nix
@@ -4,7 +4,22 @@ stdenv.mkDerivation {
   src = ./.;
   enableParallelBuilding = true;
 
-  cmakeFlags = ["-DGTEST_INCLUDE_DIR=${gtest}/include"];
+  cmakeFlags = ["-GNinja -DGTEST_INCLUDE_DIR=${gtest}/include -DBUILD_COVERAGE=ON -DBUILD_DEPENDENCIES=OFF"];
+  
+  nativeBuildInputs = [cmake ninja graphviz doxygen] ++
+    (if stdenv.isDarwin then [llvm]
+        else if stdenv.isLinux then [lcov gcc]
+        else throw "unsupported platform");
 
-  buildInputs = [gcc cmake gtest glog];
+  buildInputs = [gflags glog gtest] ;
+
+  buildPhase = ''
+    cmake --build . 
+  '';
+
+  meta = with stdenv.lib; {
+    description = "tsm, a c++ state machine framework";
+    platforms = with platforms; darwin ++ linux;
+    license = licenses.mit;
+  };
 }
